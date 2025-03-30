64c8342f8823c987ae813d3843430b4b864e7ddd
Alessio Caviglia <alepsio.ca@gmail.com>
2025-03-31 01:53:47 +0200

    Aggiornato db config.py

--- db_config.py
+++ db_config.py
@@ -1,10 +1,11 @@
 import psycopg2
+import os
 
 def get_db_connection():
     return psycopg2.connect(
-        host='metro.proxy.rlwy.net',
-        port='40745',
-        dbname='railway',
-        user='postgres',
-        password='KXXfFuNdXhksrAPXyzMahxkfqKOYmVpZ'
+        host="metro.proxy.rlwy.net",
+        port="40745",
+        database="trasporti",  # 👈 qui il tuo database reale
+        user="postgres",
+        password="KXXfFuNdXhksrAPXyzMahxkfqKOYmVpZ"  # ⚠️ esatto, quello preso da Railway
     )
